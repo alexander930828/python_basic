2775bd94d0f66234035473ce014f9d62ca6366bb
alexander930828 <77578710+alexander930828@users.noreply.github.com>
2022-12-10 23:43:26 +0900

    update4

--- for_while_break_continue.py
+++ for_while_break_continue.py
@@ -2,9 +2,43 @@
 
 # print(list(a))
 
-for i in range(10):
-    print(i)
+# for i in range(10):
+#     print(i)
 
 
-for i in range(10 ,0, -2):
+# # for i in range(10 ,0, -2):
+# #     print(i)
+
+# i=1 
+# while i<=10:
+#     print(i)
+# #     i=i+1
+
+# i=10
+
+# while i>=1:
+#     print(i)
+# #     i=i-1
+
+# i=1
+
+# while True:
+#     print(i)
+#     if i==10:
+#         break
+#     i+=1
+
+#     #축약연산자
+
+# for i in range(1, 11):
+#     if i%2==0: #짝수
+#         continue #얘가 참이면 밑에 있는건 그냥 지나가버림
+#     print(i)
+
+for i in range(1, 11):
     print(i)
+    if i>15:
+        break
+
+else:
+    print(11)
