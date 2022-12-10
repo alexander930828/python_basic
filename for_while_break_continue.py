528880c10f9bc3cf025d668ca339c86fbf522e02
alexander930828 <77578710+alexander930828@users.noreply.github.com>
2022-12-10 23:21:56 +0900

    undate2

---
+++ for_while_break_continue.py
@@ -0,0 +1,6 @@
+# a=range(1, 10)
+
+# print(list(a))
+
+for i in range(10):
+    print(i)
---
+++ viriables_and_operators.py
@@ -0,0 +1,11 @@
+# a, b = input("숫자를 입력하세요 : ").split()
+# print(type(a))
+# c = a+b
+# print(type(c))
+# print(a+b)
+
+a, b = map(int, input("숫자를 입력하세요 : ").split())
+a = int(a)
+b = int(b)
+print(a+b)
+
