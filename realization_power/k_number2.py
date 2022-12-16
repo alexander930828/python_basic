4594d2fff5fbcc038965dfa6d9de59ef8959a04b
alexander930828 <77578710+alexander930828@users.noreply.github.com>
2022-12-17 00:13:22 +0900

    Update k_number2.py

--- realization_power/k_number2.py
+++ realization_power/k_number2.py
@@ -1,5 +1,24 @@
-n = int(input())
+# n = int(input())
 
-for i in range(n):
-    N, s, e, k = map(int, input().split())
-    n
+# for i in range(n):
+#     N, s, e, k = map(int, input().split())
+#     nums = list(map(int, input().split()))
+
+#     arr=[]
+
+#     for j in range(1, N+1):
+#         if s<= j <=e:
+#             arr.append(nums[j-1])
+
+#     arr.sort()
+
+#     print(i+1, arr[k-1])
+
+
+T=int(input())
+for i in range(T):
+    n, s, e, k = map(int, input().split())
+    num = list(map(int, input().split()))
+    num=num[s-1:e]
+    num.sort()
+    print("#%d %d" %(T, num[k-1]))
