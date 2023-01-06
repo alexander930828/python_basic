f55cdf2f7af86fbae7f68102e8720fcd28c117ff
alexander930828 <77578710+alexander930828@users.noreply.github.com>
2023-01-06 13:28:40 +0900

    update25

---
+++ Stack_Queue_Deqeuue/Hash_word.py
@@ -0,0 +1,48 @@
+# n = int(input())
+
+# p = dict()
+
+# for i in range(n):
+#     word = input()
+#     p[word] = 1
+# for i in range(n-1):
+#     word = input()
+#     p[word] = 0
+
+# for key, value in p.items():
+#     if value == 1:
+#         print(key)
+#         break
+
+# n = int(input())
+
+# p = dict()
+
+# for i in range(n):
+#     word = input()
+#     p[word] = 1
+# for j in range(n-1):
+#     word = input()
+#     p[word] = 0
+
+# for key, value in p.items():
+#     if value == 1:
+#         print(key)
+#         break
+
+# 각각의 수를 카운트 할때는 딕셔너리를 쓴다.
+
+n = int(input())
+p = dict ()
+
+for i in range(n):
+    word = input()
+    p[word] = 1
+for j in range(n-1):
+    word = input()
+    p[word] = 0
+
+for key, val in p.items():
+    if val == 1:
+        print(key)
+        break
